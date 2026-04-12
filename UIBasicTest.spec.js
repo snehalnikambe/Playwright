9316284ab2ba2f11c5bbce611239df54062ef14e
snehalnikambe <32330103+snehalnikambe@users.noreply.github.com>
2026-04-12 16:33:49 +0530

    Add files via upload

---
+++ UIBasicTest.spec.js
@@ -0,0 +1,50 @@
+const {test,expect} = require('@playwright/test');
+const { sign } = require('node:crypto');
+
+
+test('Browser context playwright test',async ({browser})=>
+{
+const contex = await browser.newContext();
+const page =await contex.newPage();
+const password = page.locator('#password');
+const username = page.locator('#username');
+const SignIn= page.locator("#signInBtn");
+const cardTitles = page.locator(".card-body a");
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+console.log(await page.title());
+//await expect(page).toHaveTitle('Google');
+await username.fill("rahulshettyacademy");
+await password.fill("Learning@830$3mK2");
+await SignIn.click();
+console.log(await cardTitles.first().textContent());
+const allTitles = await cardTitles.allTextContents();
+console.log(allTitles);
+}
+);
+
+test('Page playwright test',async ({page})=>
+{
+await page.goto("https://google.com");
+await console.log(await page.title());
+await expect(page).toHaveTitle('Google');
+
+}
+);
+
+test.only('UI controls',async ({page})=>
+{
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");    
+//const dropdown = page.locator("select.form-control");
+//await dropdown.selectOption("consult");
+//await page.pause();
+//await page.locator(".checkmark").last().click();
+await page.locator(".radiotextsty").last().click();
+await page.locator("#okayBtn").click();
+await expect(page.locator(".radiotextsty").last()).toBeChecked();
+//console(page.locator(".radiotextsty").last().isChecked());
+await expect(page.locator("#terms")).toBeChecked();
+await page.locator("#terms").uncheck();
+//expect(page.locator("#terms").isChecked()).toBeFalsy();
+
+}
+);
