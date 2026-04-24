fa08f8e6aff3e62121504344d400b41f291dfd4c
Snehal <nikambe.snehal@gmail.com>
2026-04-24 15:24:32 +0530

    dropdown Code updated

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -33,8 +33,8 @@ await expect(page).toHaveTitle('Google');
 
 test('UI controls',async ({page})=>
 {
-    
-await page.goto("https://rahulshettyacademy.com/loginpagePractise/");   
+const documentLink = page.locator("[href*=documents-request]");
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/"); 
 await page.locator(".radiotextsty").last().click();
 await page.locator("#okayBtn").click();
 await expect(page.locator(".radiotextsty").last()).toBeChecked();
@@ -42,6 +42,34 @@ await page.locator("#terms").click();
 await expect(page.locator("#terms")).toBeChecked();
 await page.locator("#terms").uncheck();
 expect(await page.locator("#terms").isChecked()).toBeFalsy(); 
+const  dropdown =page.locator("select.form-control");
+await dropdown.selectOption("consult");
+await expect(documentLink).toHaveAttribute("class","blinkingText");
 
 }
 );
+
+test.only('@Child windows hadl', async ({browser})=>
+ {
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+ 
+    const [newPage]=await Promise.all(
+   [
+      context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+      documentLink.click(),
+   
+   ])//new page is opened
+   
+ 
+   const  text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain =  arrayText[1].split(" ")[0]
+    //console.log(domain);
+    await page.locator("#username").fill(domain);
+    console.log(await page.locator("#username").inputValue());
+ 
+ })
